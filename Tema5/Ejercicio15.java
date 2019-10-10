edc660112bee16ba381434f83a525fffd5a71b26
joseantoniorosales <joseantrosalesprofesional@gmail.com>
2019-10-10 10:15:39 +0200

    Añadido ejercicio 15 tema 5

--- Tema5/Ejercicio15.java
+++ Tema5/Ejercicio15.java
@@ -1,4 +1,4 @@
-public class Ejercicio14 { 
+public class Ejercicio15 { 
   public static void main(String[] args) {
     
     System.out.println("Este programa te mostrará la potencia del número introducido con el exponente que quieras");
@@ -7,31 +7,25 @@ public class Ejercicio14 {
     int base = Integer.parseInt(System.console().readLine());
     
     System.out.printf("Introduce el exponente: ");
-    int exponente = Integer.parseInt(System.console().readLine());
+    int exponenteFinal = Integer.parseInt(System.console().readLine());
     
-    double potencia = 1;
+    double potencia;
+    int exponenteInd;
     
-    if(exponente == 0)
+    
+    
+    for(int i = 1; i < exponenteFinal; i++)
     {
       potencia = 1;
-    }
-      
-    if(exponente > 0)
-    {
-      for(int i = 0; i < exponente; i++)
+      exponenteInd = i;
+    
+    
+      for(int i2 = 0; i2 > exponenteInd; i2++)
       {
         potencia *= base;
       }
-    }else if(exponente < 0)
-      {
-        for(int i = 0; i < -exponente; i++)
-        {
-          potencia *= base;
-        }
-      potencia = 1/base;
-      }
-      
     
-    System.out.printf(base + " ^ " + exponente + " = " + potencia);
+    System.out.printf(base + " ^ " + i + " = " + potencia);
+    }
   }
 }
